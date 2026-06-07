e60028b7631a8fd11aed242beb763a86c619c248
Arjun gupta <rachnag352@gmail.com>
2026-06-08 00:18:59 +0530

    completed basic banner api using redis

--- Foundation/src/controller/banner.js
+++ Foundation/src/controller/banner.js
@@ -2,13 +2,51 @@ const Redis = require("ioredis");
 const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
 
 const BANNER_KEY = "app:banner";
+
+// get banner
 const getBanner = async (req, res) => {
   try {
-    await redis.set(BANNER_KEY, "Redis in action!");
+    // if the banner exists then return it otherwise return a default message
+    const existingBanner = await redis.exists(BANNER_KEY);
+    console.log("Existing Banner:", existingBanner);
+    if (existingBanner) {
+      const banner = await redis.get(BANNER_KEY);
+      res.status(200).json({ message: banner });
+    } else {
+      res.status(404).json({ message: "Banner not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// set banner
+const setBanner = async (req, res) => {
+  try {
+    const { message } = req.body;
+    if (!message) {
+      return res.status(400).json({ error: "Message is required" });
+    }
+    await redis.set(BANNER_KEY, message);
     res.status(200).json({ message: "Banner set successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-module.exports = { getBanner };
+// delete banner
+const deleteBanner = async (req, res) => {
+  try {
+    const existingBanner = await redis.exists(BANNER_KEY);
+    if (existingBanner) {
+      await redis.del(BANNER_KEY);
+      res.status(200).json({ message: "Banner deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Banner not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getBanner, setBanner, deleteBanner };
--- Foundation/src/route/banner.js
+++ Foundation/src/route/banner.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const router = Router();
-const { getBanner } = require("../controller/banner");
+const { getBanner, setBanner, deleteBanner } = require("../controller/banner");
 router.get("/banner", getBanner);
+router.post("/banner", setBanner);
+router.delete("/banner", deleteBanner);
 module.exports = router;
